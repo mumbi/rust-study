33a7c0b93fdbb23a5f5a25711bf1fa2d5ffc7bbc
mumbi <mumbi@daum.net>
2023-07-15 13:36:12 +0000

    [3] week 2 1일차 오후

---
+++ src/commands.rs
@@ -0,0 +1,85 @@
+use std::io;
+
+pub struct Command {
+    name: String,
+    handler: fn()
+}
+
+impl Command {
+    pub fn new(name: &str, handler: fn()) -> Command {
+        Command {
+            name: String::from(name),
+            handler: handler
+        }
+    }
+
+    pub fn handle(&self) {
+        (self.handler)();
+    }
+}
+
+pub struct CommandHandler {
+    pub name: String,
+    commands: Vec<Command>
+}
+
+impl CommandHandler {
+    pub fn new<T>(name: &str, iter: T) -> CommandHandler
+    where T: Iterator<Item = Command> {
+        CommandHandler {
+            name: String::from(name),
+            commands: Vec::from_iter(iter)
+        }
+    }
+
+    pub fn handle(&self) {
+        let result = self.select();
+    
+        match result {
+            Err(error) => {
+                println!("{error}");
+                return;
+            },
+            Ok(selected_command) => {
+                match selected_command {
+                    None => {
+                        println!("{} is empty.", self.name);
+                        return;
+                    },
+                    Some(selected_command) => {
+                        selected_command.handle();
+                    }
+                }
+            }
+        }
+    } 
+
+    fn select(&self) -> Result<Option<&Command>, String> {
+        println!("{}", self.name);
+
+        if self.commands.is_empty() {
+            return Ok(None);
+        }
+
+        for i in 0..self.commands.len() {
+            let no = i+1;
+            
+            println!("{}) {}", no, self.commands[i].name);
+        }
+
+        let mut input_string = String::new();
+
+        io::stdin().read_line(&mut input_string).expect("failed to readline.");
+        
+        let selected_no = input_string.trim().parse::<usize>().unwrap();
+
+        if self.commands.len() < selected_no {
+            return Err(format!("selected no ({selected_no}) is invalid."));
+        }
+
+        let command = &self.commands[selected_no - 1];
+    
+        return Ok(Some(command));
+    }
+}
+
---
+++ src/game.rs
@@ -0,0 +1,4 @@
+
+pub fn run_game() {
+    println!("run game");
+}
